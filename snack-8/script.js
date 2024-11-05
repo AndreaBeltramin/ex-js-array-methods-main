9e70f57fc07358b64198b0c3ee448006e0fe8ef6
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-05 17:11:51 +0100

    correzione snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -22,8 +22,8 @@ const students = [
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
 
-const studentMarco = students.find((student, index) => {
-	return (students.name = "Marco Lanci");
+const studentMarco = students.find((student) => {
+	return student.name === "Marco Lanci";
 });
 console.log(studentMarco);
 console.log(studentMarco.class);
